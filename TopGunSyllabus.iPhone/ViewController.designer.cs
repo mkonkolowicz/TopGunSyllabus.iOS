67083334c86122dd6d4543b0902222799c6823de
Maciej Konkolowicz <mkonkolowicz@hotmail.com>
2016-05-05 21:58:29 -0400

    Added stuff to main storyboard

--- TopGunSyllabus.iPhone/ViewController.designer.cs
+++ TopGunSyllabus.iPhone/ViewController.designer.cs
@@ -1,17 +1,37 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace TopGunSyllabus.iPhone
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel TopGunBanner { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView TopGunBannerView { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (TopGunBanner != null) {
+				TopGunBanner.Dispose ();
+				TopGunBanner = null;
+			}
+			if (TopGunBannerView != null) {
+				TopGunBannerView.Dispose ();
+				TopGunBannerView = null;
+			}
+		}
+	}
 }
